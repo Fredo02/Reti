7db4ed2c369318ae1daf880a1699b622cc2e3292
Alfredo <segalaalfredo@gmail.com>
2024-04-17 21:16:06 +0200

    Modified transit

--- transit/transit.c
+++ transit/transit.c
@@ -69,6 +69,7 @@ void transit(int binari){
 
     for(int i = 0; i < N; i++){
         pthread_mutex_lock(&mutex_attraversano);
+        attesa--;
         struct sembuf sem_op = {i, -1, 0};
 
         ret = semop(binari, &sem_op, 1);
@@ -86,6 +87,7 @@ void transit(int binari){
             exit(1);
         }
 
+        presente--;
         passato++;        
         pthread_mutex_unlock(&mutex_attraversano);
     }
